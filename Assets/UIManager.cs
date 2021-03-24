fd5440f8096367903950cba5ac438cd0b935f000
xuongthin <xuongthin2000@gmail.com>
2021-03-24 10:34:07 +0700

    first commit

---
+++ Assets/UIManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public void CheckButtonClicked() {
+        Debug.Log("clicked");
+    }
+}
